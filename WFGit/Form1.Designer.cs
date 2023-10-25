5292c329c39cc4c503e8abf39a2bb52168508ec2
Arthur <tupaartur27@gmail.com>
2023-10-25 21:36:23 +0300

    Add random quotes generations

--- WFGit/Form1.Designer.cs
+++ WFGit/Form1.Designer.cs
@@ -29,32 +29,47 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            btnPush = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.BackColor = Color.FromArgb(255, 255, 192);
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(43, 81);
+            label1.Location = new Point(50, 28);
             label1.Name = "label1";
             label1.Size = new Size(681, 243);
             label1.TabIndex = 0;
             label1.Text = "Hello Word";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // btnPush
+            // 
+            btnPush.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnPush.Location = new Point(334, 306);
+            btnPush.Name = "btnPush";
+            btnPush.Size = new Size(125, 50);
+            btnPush.TabIndex = 1;
+            btnPush.Text = "Push";
+            btnPush.UseVisualStyleBackColor = true;
+            btnPush.Click += btnPush_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnPush);
             Controls.Add(label1);
             Name = "Form1";
             Text = "GitHub Test";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
         #endregion
 
         private Label label1;
+        private Button btnPush;
     }
 }
--- WFGit/Form1.cs
+++ WFGit/Form1.cs
@@ -2,9 +2,31 @@ namespace WFGit
 {
     public partial class Form1 : Form
     {
+        List<string> quotes;
+        Random random;
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            label1.Text = quotes[random.Next(quotes.Count)];
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                quotes = (await File.ReadAllLinesAsync("quotes.txt")).ToList();
+            }
+            catch (Exception ex)
+            {
+
+               MessageBox.Show(ex.Message);
+            }
+            
         }
     }
 }
